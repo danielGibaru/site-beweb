94c8aefd96794eb4c77784e6507440935f2a2e1e
Gibaru Daniel <daniel.gibaru.34@gmail.com>
2017-04-27 16:21:39 +0200

    espace user

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new BewebBundle\BewebBundle(),
             new AdminBundle\AdminBundle(),
+            new UserBundle\UserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
