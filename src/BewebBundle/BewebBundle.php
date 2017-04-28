6bed6e35e1c7b4d57789641e1d9323b4b3240809
Corin Alexandru <corin.alex@gmail.com>
2017-04-28 08:13:42 +0200

    restructuration, nettoyage, amélioration espace admin

--- src/BewebBundle/BewebBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BewebBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BewebBundle extends Bundle
-{
-}
--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/apprenant")
+     * @Route("/utilisateur")
      */
     public function indexAction()
     {
